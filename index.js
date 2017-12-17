4305442375a3dfd52407b6e56b241b833cda1702
dev-skoval <dev.skoval@gmail.com>
2017-12-17 12:35:30 +0200

    Create server

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 "use strict";
 
-const koa = require("koa");
+const Koa = require("koa");
+const app = new Koa();
+const port = process.env.PORT || 3000;
+
+
+
+app.listen(port, (err) => {
+    console.log(`Server is up on port ${port}`)
+});
